910ee182da85ec61980ac47a9d36cdfe5cf1f0be
Rakesh Choudhary <seervir987@gmail.com>
2025-01-19 12:22:07 +0530

    Design Student,Teacher and parent home layout.

--- app/(dashboard)/parent/page.tsx
+++ app/(dashboard)/parent/page.tsx
@@ -2,12 +2,12 @@ import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
 import EventCalendar from "@/components/EventCalendar";
 
-const TeacherPage = () => {
+const ParentPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule</h1>
+          <h1 className="text-xl font-semibold">Schedule (Rakesh)</h1>
           <BigCalendar />
         </div>
       </div>
@@ -20,4 +20,4 @@ const TeacherPage = () => {
   );
 };
 
-export default TeacherPage;
+export default ParentPage;
